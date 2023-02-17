dc144d40fc2e1ff78b9a646fa1be48610cfb8ac7
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-17 10:00:31 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-//the puts function was used as an alternative to printf 
+
+/*the puts function was used as an alternative to printf*/
+
 int main(void) {
 	puts("Programming is like buildinga multilingual puzzle\n");
 	return(0);
