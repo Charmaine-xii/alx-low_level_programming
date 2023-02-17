643d04036be4c66a9216302226750c49d1796655
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-17 12:30:13 -0800

    Quote c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing a new line and compiling a file to the standard error
+ *
+ * Return: 1 (Succesful)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	return (1);
+}
