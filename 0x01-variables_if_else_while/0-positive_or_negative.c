ff4c621f8484438ddcaec87b0e1e1963cf758eb0
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-18 18:56:55 +0200

    Postive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - assigning a random number to n variable
+*
+* Return: 0 (successful)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is postive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
+
