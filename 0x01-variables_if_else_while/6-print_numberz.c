3e55b86750bf084e70fe2573e5fc5478c9e72ad4
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-19 16:52:15 +0200

    Print numberz using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base 10 starting with 0
+*
+* Return: 0 (Successful
+*/
+int main(void)
+{
+	int num;
+
+	while (num = 0; num < 10; num++)
+	{
+		putchar("%d", num);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
