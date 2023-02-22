4cc090d4f28e85526e000543331d26e2eaf05e9a
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-23 00:03:40 +0200

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_to_98 - prints all natural numbers from n to 98
+* @n: the number to start counting at
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			_putchar("%d, ", n--);
+		_putchar("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			_putchar('%d, ", n++);
+		_putchar("%d\n", n);
+	}
+}
