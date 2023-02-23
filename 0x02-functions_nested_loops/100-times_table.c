10317b1a52df544ec777a752e9448e367b8a2606
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-23 05:32:30 +0200

    Print to 98

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_times_table - prints the n times table starting with 0
+*
+* @n: the value of the times table to be printed.
+*
+*/
+void print_times_table(int n)
+{
+
+	return (0);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,33 +5,18 @@
 */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n >= 98)
 	{
-		while (n <= 98)
-		{
-			_putchar('%d', n);
-			if (n != 98)
-			{
-				_putchar(',');
-			}
-			n++;
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			_putchar('%d', n);
-			if (n != 98)
-			{
-				_putchar(',');
-			}
-			n--;
-		}
-	}
+
 	else
 	{
-		putchar('98');
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-	_putchar('\n');
 }
+
