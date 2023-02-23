8f4ec428c9d004a643a7c84ab48b0b3b76636a0a
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-23 05:41:11 +0200

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,36 @@
 */
 void print_times_table(int n)
 {
+	int num, mult, prod;
 
-	return (0);
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			for (mult = 0; mult <= n; mult++)
+			{
+				prod = num * mult;
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+				}
+				else if (prod < 100 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+
+				if (mult == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 100)
+					_putchar(' ');
+				if (prod < 10)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
