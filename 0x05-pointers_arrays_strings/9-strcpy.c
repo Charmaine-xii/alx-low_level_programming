cafcb476b77977a2f9acf73e2a096169cdf1b81e
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-28 01:03:11 +0200

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,17 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int x = 0;
 
-	while (src[i])
+	while (*(src + 1) != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		l++;
 	}
+	for (; x < 1; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\n';
 
 	return (dest);
 }
