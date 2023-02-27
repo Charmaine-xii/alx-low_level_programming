ef6c9037ac44c4b8abed072355ea750e1376b00d
Charmaine-xii <charmainemohlala12@gmail.com>
2023-02-27 23:52:20 +0200

    Every second letter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of string, starting with first char
+* @str: string containing characters
+*/
+void puts2(char *str)
+{
+	int i = 0;
+	int slth = 0;
+
+	while (str[i++])
+		slth++;
+
+	for (i = 0; i < slth; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n')
+}
