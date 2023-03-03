985432d8dbc6c1152cd378f6e3e6ccba62cc9c16
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-04 01:43:39 +0200

    String to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* string_toupper - chnages all lowercase letters of a string to uppercase
+* @str: the string
+* Return: the string
+*/
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z'
+			str[i] = (int)str[i] - 32;
+	}
+
+	return (str);
+}
