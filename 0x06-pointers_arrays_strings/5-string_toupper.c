04e5499348c9ee299cca4f3ea221d3353a746bff
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-04 01:53:22 +0200

    String to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,11 +7,12 @@
 */
 char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i])
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z'
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
 
 		i++
