24b2e1e71fd9f508c51132556aa4919869c3ed21
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-03 14:07:46 +0200

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
 * @dest: string to be concatenated
 * Return: pointer to the resulting string @dest
 */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 
 	int i;
