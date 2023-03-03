3c64196120b76667366b95e1f6cdd5ea2398fa7d
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-03 20:23:17 +0200

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,11 @@ char *_strcat(char *dest, const char *src)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -24,5 +24,6 @@ char *_strcat(char *dest, const char *src)
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
 }
