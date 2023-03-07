7a912e93722e3cf2fb20b1708d8c856225958d40
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-07 23:26:52 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,14 @@
 */
 char *_strchr(char *s, char c)
 {
+	int i;
 
-	while (*s != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
+		if (*s[i] == c)
+			return (s + i);
 
-		s++;
 	}
 
-	return (NULL)
+	return ('\0')
 }
