6ccb841bf8bf4dc7af8f719c084adca59a713046
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-07 22:45:15 +0200

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i] /* destination = source of memory area */
+		dest[i] = src[i]; /* destination = source of memory area */
 	}
 
 	return (dest);
