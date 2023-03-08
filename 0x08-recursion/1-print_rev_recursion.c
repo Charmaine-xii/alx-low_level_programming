809ae1eefa2ce4acf93600977ac58c5ffc4de2ac
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-08 22:03:04 +0200

    print reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - prints string in reverse
+* @s: string to be printed in reverse
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recusion(s + 1); /* we first reverse the string */
+		_putchar(*s);		/* we print after reversing */
+	}
+}
