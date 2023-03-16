f2a6c70e82445b212a5b28dbc7fc59cd2f085626
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-16 21:44:50 +0200

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * string_nconcat - concatenates two strings
-* @s1: first string
-* @s2: second string
-* @n: maximum number of bytes of @s2 to concatenated to @s1
+* @s1: string to add to
+* @s2: string to put after @s1 inconcatenates string
+* @n: number of chars to concat from @s2
 *
-* Return: pointer to concatenated space in memory,
-*	Otherwise if function fails return NULL
+* Return: new string if passed, NULL if failed
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+void *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, len1, len2;
-	char *constr;
+	char *newstr;
 
 	j = 0;
 
@@ -22,26 +23,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = (int)len1(s1);
-	len2 = (int)len2(s2);
+	len1 = (int)strlen(s1);
+	len2 = (int)strlen(s2);
 	if (n > len2)
 		n = len2;
 
-	constr = malloc(sizeof(char) * (len1 + n + 1));
+	newstr = malloc(sizeof(char) * (len1 + n + 1));
 
-	if (constr == NULL)
+	if (newstr == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
-		constr[j] = s1[i];
+		newstr[j] = s1[i];
 		j++;
 	}
 	for (i = 0; i < n; i++)
 	{
-		constr[j] = s2[i];
+		newstr[j] = s2[i];
 		j++;
 	}
-	constr[j] = '\0';
+	newstr[j] = '\0';
 
-	return (constr);
+	return (newstr);
 }
