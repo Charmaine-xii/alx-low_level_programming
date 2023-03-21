e8e786ac5efa90e5389ea3ae102e64e569106cb4
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-21 18:44:47 +0000

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
