f66da7c03dae276ffb75e7eb25c4cedee34ada0f
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-21 19:16:08 +0000

    third commit of function that initialize a variable of type struct dog, fixed betty issue

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,12 +8,14 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
+ * Return: 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
