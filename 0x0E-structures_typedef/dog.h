f40897f4348bcd06cedf77bc439fc4af15754ceb
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-22 08:29:49 +0200

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 /**
 * dog_t - Typedef for struct
 */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
