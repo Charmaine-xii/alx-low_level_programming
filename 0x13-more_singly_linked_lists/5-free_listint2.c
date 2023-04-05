27e3b1c5bdf4acbacff1f07d9585b8d51afdd88e
Charmaine-xii <charmainemohlala12@gmail.com>
2023-04-05 06:21:28 +0000

    Free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a list_t list including all values inside
+* @head: pointer to the head of the list to be freed
+*
+* Description: go through the list while head is not NULL
+*				free the integer element, then free the object itself
+*				then assign next of that element to next of head
+*				then free head after everything is done
+* Return: Nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
