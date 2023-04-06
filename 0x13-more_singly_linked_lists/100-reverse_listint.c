8b4e01b5443e63b04bb473570fd04ee2d42bdf0e
Charmaine-xii <charmainemohlala12@gmail.com>
2023-04-06 06:26:28 +0000

    reverse list; fixed segmentation fault

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,9 +1,22 @@
 #include "lists.h"
 
-
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: pointer to the head
+ *
+ * Description: if head is NULL, return NULL,
+ *				if next is NULL, return head as is
+ *				loop through list while node is not NULL
+ *				set next node as the next of the current
+ *				set current node's next to the previous node
+ *				set the previous node to the current node
+ *				set current node to next node
+ *
+ * Return: pointer to the first node of reversed list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev_node = NULL, *next_node = NULL, *current_node = *head;
+	listint_t *next_node = NULL, *prev_node = NULL;
 
 	if (*head == NULL || head == NULL)
 		return (NULL);
@@ -11,13 +24,11 @@ listint_t *reverse_listint(listint_t **head)
 		return (*head);
 	while (*head)
 	{
-		next_node = current_node->next;
-		current_node->next = prev_node;
-		prev_node = current_node;
-		current_node = next_node;
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = (*head);
+		*head = next_node;
 	}
-
 	*head = prev_node;
 	return (*head);
 }
-
