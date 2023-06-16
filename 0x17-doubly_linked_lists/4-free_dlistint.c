1dc220b72e6c075e1a06c5aa33576b0cb7d649c9
Charmaine-xii <charmainemohlala12@gmail.com>
2023-06-16 11:26:12 -0700

    Free dlistint

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,8 @@
 #include "lists.h"
 
 /**
-* free_dlistint_t - frees a dlistint_t list including all values inside
+* free_dlistint - frees a dlistint_t list including all values inside
 * @head: pointer to the head of the list to be freed
-*
-* Return: Nothing
 */
 void free_dlistint(dlistint_t *head)
 {
