101a204217078aad513a9297175919bc7a5937a0
Charmaine-xii <charmainemohlala12@gmail.com>
2023-12-17 04:32:55 -0800

    Fixed return value of malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (ht == NULL) {
 		return (NULL);
+	}
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
